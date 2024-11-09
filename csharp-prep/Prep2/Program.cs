4264925724568097ba513d26b7fecd2d830414a6
hlaolil <hla24002@byui.edu>
2024-11-09 22:02:00 +0200

    Prep2 core requirements met

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string grade = "";
+        Console.Write("What is your grade percentage? ");
+        int percentage = int.Parse(Console.ReadLine());
+
+        if (percentage >= 90)
+        {
+            grade = "A";
+        }  
+        else if (percentage >= 80)
+        {
+           grade = "B";
+        }
+        else if (percentage>= 70)
+        {
+            grade= "C";
+        }
+        else if (percentage>= 60)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "F";
+        }
+        Console.WriteLine($"Your grade is {grade}");
+        if (grade == "A" || grade == "B" || grade == "C")
+        {
+            Console.WriteLine("Congratulations, You have Passed");
+        }
+        else 
+        {
+            Console.WriteLine("Sorry, you have not passed, try harder next time");
+        }
+
     }
 }
