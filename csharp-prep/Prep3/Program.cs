72240b9eaa27496732438adf7e425cd14093090c
hlaolil <hla24002@byui.edu>
2024-11-10 17:49:08 +0200

    prep3 core requirements met

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magic = randomGenerator.Next(1,100);
+        int guess = 0;
+        while (guess != magic)
+        {
+            Console.Write("Enter Guess Number between 1 and 100: ");
+            if (!int.TryParse(Console.ReadLine(), out guess))
+            {
+                Console.WriteLine("Please enter a valid integer.");
+                continue;
+            }
+            if (guess < magic)
+            {
+                Console.WriteLine("Guess higher");
+            }
+            else if (guess > magic)
+            {
+                Console.WriteLine("Guess lower");
+            }
+            else
+            {
+            Console.WriteLine("You guessed it! ");
+            }
+        }
     }
+        
+        
 }
