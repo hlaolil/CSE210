b24c756a4cbcc61506a56bcb75a4038992f170a1
hlaolil <hla24002@byui.edu>
2024-11-13 23:03:32 +0200

    wip

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,14 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
+        job1 = new job()
         job1._jobTitle = "Pharmacist";
         job1._company = "Naleli";
         job1._startYear = 2015;
         job1._endYear = 2025;
+        job1.JobInfo();
 
+        job2 = new job()
         job2._jobTitle = "Pastor";
         job2._company = "AGL";
         job2._startYear = 2021;
         job2._endYear = 2025;
+        job2.JobInfo();
     }
 }
