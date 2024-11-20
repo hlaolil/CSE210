c38671fcfd0833fa5bf11f0f37c793f8ff4caaa6
hlaolil <hla24002@byui.edu>
2024-11-20 22:55:20 +0200

    Learning03 started

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,33 @@
+
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top
+        _bottom =bottom
+    }
+
+    public int top
+    {
+        get{}
+        set{}
+    }
+
+    public int bottom
+    {
+        get{}
+        set{}
+    }
+
+    public void GetFractionString()
+    {
+
+    }
+    public int GetDecimalValue()
+    {
+        
+    }
+}
