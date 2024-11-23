530ad36c57cce7ff991c293297a7ccbcff13a6db
hlaolil <tlokza@gmail.com@>
2024-11-23 15:29:46 +0200

    wip

---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,17 @@
+
+
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+    
+    public void HideRandomWords(int numberToHide )
+    {
+
+    }
+    public string GetDisplayText()
+    {
+
+    }
+    public bool IsCompletelyHidden()
+}
---
+++ prove/Develop03/Word.cs
@@ -0,0 +1,25 @@
+
+
+
+public class Word
+{
+    private string _text;
+    private bool _isHidden;
+    
+    public void Hide()
+    {
+        
+    }
+    public void Show()
+    {
+
+    }
+    public bool IsHidden()
+    {
+
+    }
+    public string GetDisplayText()
+    {
+    
+    } 
+}
