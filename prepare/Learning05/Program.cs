fe79f35fdaec14c9fcb05aea21246829a6985b8d
hlaolil <167699704+hlaolil@users.noreply.github.com>
2024-12-10 08:33:03 +0200

    Update Program.cs

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Assignment assignment = new Assignment("Business as Mission", "Letlotlo Hlaoli");
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment mathAssignment = new MathAssignment("Math", "Letlotlo Hlaoli", "Chapter 5", "1-10");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WrittenAssignment writtenAssignment = new WrittenAssignment("History", "Letlotlo Hlaoli", "The Rise of Civilizations");
+        Console.WriteLine(writtenAssignment.GetSummary());
+        Console.WriteLine(writtenAssignment.GetWritingInfo());
+
     }
 }
