bd4948d6172314e312873687f6d234ea48fc9463
hlaolil <167699704+hlaolil@users.noreply.github.com>
2024-12-10 08:30:41 +0200

    Update MathAssignment.cs

--- prepare/Learning05/MathAssignment.cs
+++ prepare/Learning05/MathAssignment.cs
@@ -1,9 +1,34 @@
 using System;
 
-class Program
+class MathAssignment : Assignment
 {
-    static void Main(string[] args)
+    private string _textbookSection;
+    private string _problems;
+
+    // Constructor
+    public MathAssignment(string topic, string studentName, string textbookSection, string problems)
+        : base(topic, studentName)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        _textbookSection = textbookSection;
+        _problems = problems;
+    }
+
+    // Properties
+    public string TextbookSection
+    {
+        get { return _textbookSection; }
+        set { _textbookSection = value; }
+    }
+
+    public string Problems
+    {
+        get { return _problems; }
+        set { _problems = value; }
+    }
+
+    // Method to get homework list
+    public string GetHomeworkList()
+    {
+        return $"Textbook Section: {TextbookSection}, Problems: {Problems}";
     }
 }
