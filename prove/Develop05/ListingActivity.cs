20c279646203a8131fbed377dc2bc0dfc26ca6f8
hlaolil <167699704+hlaolil@users.noreply.github.com>
2024-12-11 18:02:52 +0200

    Create ListingActivity.cs

---
+++ prove/Develop05/ListingActivity.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+
+
+
+public class ListingActivity : Activity
+{
+    private int _count;
+    private List<string> _prompts;
+
+    public ListingActivity(string name, string description, int duration, List<string> prompts) : base(name, description, duration)
+    {
+        _prompts = prompts;
+    }
+
+    public void Run()
+    {
+        DisplayStartingMessage();
+        string prompt = GetRandomPrompt();
+        Console.WriteLine($"Prompt: {prompt}");
+        Console.WriteLine("List as many responses as you can:");
+        var responses = GetListFromUser();
+        _count = responses.Count;
+        Console.WriteLine($"You listed {_count} items.");
+        DisplayEndingMessage();
+    }
+
+    private string GetRandomPrompt()
+    {
+        Random rand = new Random();
+        return _prompts[rand.Next(_prompts.Count)];
+    }
+
+    private List<string> GetListFromUser()
+    {
+        List<string> responses = new List<string>();
+        DateTime endTime = DateTime.Now.AddSeconds(Duration);
+        while (DateTime.Now < endTime)
+        {
+            string response = Console.ReadLine();
+            if (!string.IsNullOrEmpty(response))
+                responses.Add(response);
+        }
+        return responses;
+    }
+}
