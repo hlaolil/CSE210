da1426166a20d5d3add9ba7508cd2c2b9cff976f
hlaolil <hla24002@byui.edu>
2024-12-12 18:44:35 +0200

    wip

--- prove/Develop05/Activity.cs
+++ prove/Develop05/Activity.cs
@@ -47,7 +47,7 @@ public class Activity
         for (int i = 0; i < seconds; i++)
         {
             Console.Write("|\b/\b-\b\\\b");
-            System.Threading.Thread.Sleep(250);
+            System.Threading.Thread.Sleep(500);
         }
         Console.WriteLine();
     }
