a2c3b8294430ac90aa55c5f082d8128e35768f64
hlaolil <hla24002@byui.edu>
2024-12-18 18:47:04 +0200

    wip

--- foundation/Foundation3/Program.cs
+++ foundation/Foundation3/Program.cs
@@ -1,9 +1,22 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation3 World!");
+       
+        var activities = new List<Activity>
+        {
+            new Running(new DateTime(2022, 11, 3), 30, 4.8), // Distance in km
+            new Cycling(new DateTime(2022, 11, 4), 45, 20),  // Speed in kph
+            new Swimming(new DateTime(2022, 11, 5), 40, 30)  // Laps
+        };
+
+      
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
